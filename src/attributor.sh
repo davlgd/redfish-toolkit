a905fa7cd7b7aedcf50b83d906b745ebaec95b5e
davlgd <1110600+davlgd@users.noreply.github.com>
2023-11-04 21:40:49 +0100

    Fix & quicker detection

--- src/attributor.sh
+++ src/attributor.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+# Attributor v0.1
 echo -ne '\033]0;Attributor v0.1\007'
+echo "Attributor v0.1"
 
-# Check for required tools
-command -v curl >/dev/null 2>&1 || { echo >&2 "curl is not installed. Exiting."; exit 1; }
-command -v xclip >/dev/null 2>&1 || command -v pbpaste >/dev/null 2>&1 || command -v gpaste >/dev/null 2>&1 || { echo >&2 "Clipboard tools (xclip, pbpaste, gpaste) are not available. Exiting."; exit 1; }
-
-# Show helper
-helper() {
+display_help() {
     echo "Usage: $0 [OPTIONS]"
     echo
     echo "Options:"
@@ -15,71 +12,62 @@ helper() {
     echo "  --file-ip=FILENAME    Specify the IP file. Default is 'ip.txt'. Ignored if --ip-range is set."
     echo "  --max-tries=N         Specify maximum number of tries. Default is 10."
     echo "  --delay=N             Specify delay between each round in seconds. Default is 60."
-    echo "  --endpoint=PATH       Specify the endpoint path. Default is '/redfish/v1/Chassis'"
+    echo "  --endpoint=PATH       Specify the endpoint path. Default is '/redfish/v1/Systems'"
     echo "  --username=NAME       Specify the username for authentication. Default is 'admin'"
-    echo "  --encrypt             Encrypt the passwords in the output using a AES and a passphrase"
     echo "  --help                Display this help and exit."
     echo
 }
 
 # Defaults
 FILE_IP="ip.txt"
-OUTPUT_FILE="auth.crypt"
-ENCRYPT=false
 MAX_TRIES=10
 DELAY=60
-ENDPOINT_PATH="/redfish/v1/Chassis"
+ENDPOINT_PATH="/redfish/v1/Systems"
 USERNAME="admin"
 
-# Handle arguments
-while [ "$#" -gt 0 ]; do
-    case $1 in
+# No arguments case
+if [[ $# -eq 0 ]]; then
+    display_help
+    exit 0
+fi
+
+# Argument Parsing for --help
+for arg in "$@"; do
+    case $arg in
         --help)
-            helper
-            exit 0
-            ;;
-        --encrypt)
-            ENCRYPT=true
-            shift
-            ;;
-        --ip-range=*)
-            IP_RANGE="${1#*=}"
-            shift
-            ;;
-        --file-ip=*)
-            FILE_IP="${1#*=}"
-            shift
-            ;;
-        --max-tries=*)
-            MAX_TRIES="${1#*=}"
-            shift
-            ;;
-        --delay=*)
-            DELAY="${1#*=}"
-            shift
-            ;;
-        --endpoint=*)
-            ENDPOINT_PATH="${1#*=}"
-            shift
-            ;;
-        --username=*)
-            USERNAME="${1#*=}"
-            shift
-            ;;
-        *)
-            echo "Unrecognized option: $1"
-            helper
-            exit 1
-            ;;
+        display_help
+        exit 0
+        ;;
     esac
 done
 
-if $ENCRYPT; then
-    echo -n "Enter an encryption 🔐: "
-    read -rs ENCRYPTION_PASSPHRASE
-    echo
-    > $OUTPUT_FILE
-fi
+# Argument Parsing for other arguments
+for key in "$@"; do
+    case $key in
+        --ip-range=*)
+        IP_RANGE="${key#*=}"
+        ;;
+        --file-ip=*)
+        FILE_IP="${key#*=}"
+        ;;
+        --max-tries=*)
+        MAX_TRIES="${key#*=}"
+        ;;
+        --delay=*)
+        DELAY="${key#*=}"
+        ;;
+        --endpoint=*)
+        ENDPOINT_PATH="${key#*=}"
+        ;;
+        --username=*)
+        USERNAME="${key#*=}"
+        ;;
+        *)
+        echo "Unknown argument: $key"
+        exit 1
+        ;;
+    esac
+done
 
 # IP List Creation
 if [[ -n $IP_RANGE ]]; then
@@ -112,36 +100,31 @@ fi
 
 function detect_passwords {
     # Check clipboard utilities
-    # TODO : Redundant, to simplify  
     if command -v xclip >/dev/null 2>&1; then
         PASSWORDS=$(xclip -selection clipboard -o)
     elif command -v pbpaste >/dev/null 2>&1; then
         PASSWORDS=$(pbpaste)
-    elif command -v gpaste >/dev/null 2>&1; then
-        PASSWORDS=$(gpaste)
+    elif command -v clip.exe >/dev/null 2>&1; then
+        PASSWORDS=$(clip.exe)
     else
-        echo "Error: No clipboard utility detected. Install Gpaste, pbpaste, or xxlip."
+        echo "Error: No clipboard utility detected. Install xclip, pbpaste, or clip.exe."
         exit 1
     fi
 
     # Transform the clipboard data into an array
     IFS=$'\n' read -d '' -r -a PASSWORD_ARRAY <<< "$PASSWORDS"
-
-    # Clear the previous line
-    echo -ne "\033[1A" # Move to the previous line
-    echo -ne "\033[K"  # Clear the current line
 }
 
 detect_passwords
 
 while true; do
     NUM_PASSWORDS=${#PASSWORD_ARRAY[@]}
-    echo -ne "$NUM_PASSWORDS 🔐 detected in your clipboard. Do you want to continue, detect again, or exit (c/d/e)? "
+    echo -ne "$NUM_PASSWORDS 🔐 detected. Do you want to continue, detect again, or exit (y/n/d)? "
     read -r choice
 
     case "$choice" in
-      c|C ) break;;
-      e|E ) echo "Exiting..."; exit 1;;
+      y|Y ) break;;
+      n|N ) echo "Exiting..."; exit 1;;
       d|D ) 
             echo -ne "\033[2K\r" # Clear the current line
             detect_passwords
@@ -156,13 +139,7 @@ echo "Starting the process to find matching IP:password pairs..."
 
 try_count=0
 
-# Simple encryption using OpenSSL with AES-256 encryption with the passphrase provided by the user
-function encrypt_password {
-    echo "$1" | openssl enc -aes-256-cbc -salt -pbkdf2 -a -pass pass:$ENCRYPTION_PASSPHRASE
-}
-
-while [[ ${#IPs[@]} -gt 0 && 
-${#PASSWORD_ARRAY[@]} -gt 0 && $try_count -lt $MAX_TRIES ]]; do
+while [[ ${#IPs[@]} -gt 0 && ${#PASSWORD_ARRAY[@]} -gt 0 && $try_count -lt $MAX_TRIES ]]; do
     for ip_idx in "${!IPs[@]}"; do
         ip=${IPs[$ip_idx]}
         for pass_idx in "${!PASSWORD_ARRAY[@]}"; do
@@ -172,22 +149,22 @@ ${#PASSWORD_ARRAY[@]} -gt 0 && $try_count -lt $MAX_TRIES ]]; do
             if [[ -z "$password" ]]; then
                 continue
             fi
-
+ 
             ENDPOINT="https://$ip$ENDPOINT_PATH"
-            response=$(curl -k -s -o /dev/null -w "%{http_code}" -X GET $ENDPOINT \
+            response=$(curl -k -s -o /dev/null -w "%{http_code}" --connect-timeout 1 -X GET $ENDPOINT \
+                -H "Content-Type: application/json" \
                 -u $USERNAME:$password)
 
             if [ "$response" == "200" ]; then
-                if $ENCRYPT; then
-                    echo "$ip:✅"   
-                    echo "$ip:$(encrypt_password $password)" >> $OUTPUT_
-                FILE
-                else
-                    echo "$ip:$password"
-                fi
-
+                echo "$ip:$password"
+                
                 unset IPs[$ip_idx]
                 unset PASSWORD_ARRAY[$pass_idx]
+                break
+            fi
+           
+            if [ "$response" == "000" ]; then
+                unset IPs[$ip_idx]
                 break
             fi
         done
